916d9f5c2508212d55fd5e6a830c300d364f4b3d
Carlos Santana <carlos.santanag@live.cl>
2024-09-30 20:26:49 -0300

    v1.4 - Descomento lineas db

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,5 +10,6 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-# bundle exec rails db:migrate
-# bundle exec rails db:seed
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
