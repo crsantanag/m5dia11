ea930827f501b4f72373a86f590b4723a3e6e7da
Carlos Santana <carlos.santanag@live.cl>
2024-09-30 19:51:02 -0300

    v1.2 - Listo para deploy

--- config/routes.rb
+++ config/routes.rb
@@ -13,3 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "avatars#index"
 end
+"Agregando controlador con acción index y captura de avatars.
+Además se modifica el seed.rb"
