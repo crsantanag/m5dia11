5f01a87841853078eccd86ec7582f6d3dbe93077
Carlos Santana <carlos.santanag@live.cl>
2024-10-01 13:18:56 -0300

    v2.3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,3 +9,6 @@ bundle exec rails assets:clean
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
+
+bundle exec rails db:migrate
+bundle exec rails db:seed
